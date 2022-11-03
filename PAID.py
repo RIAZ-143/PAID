9bcf38915dec3b5271bf1bb69363742c3bb460e4
MR.RIAZ <109266957+RIAZ-143@users.noreply.github.com>
2022-11-03 19:37:25 +0500

    Create PAID.py

---
+++ PAID.py
@@ -0,0 +1,21 @@
+import os, platform
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+os.system('git pull')
+
+import requests
+
+os.system('git pull')
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    import PAID
